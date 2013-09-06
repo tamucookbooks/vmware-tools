d7c116ba7a565ac937eb7d30f1ab5d695769e04e
cgjohnson <chuckjohn@tamu.edu>
2013-09-06 14:06:16 -0500

    adding centos support

--- recipes/binary.rb
+++ recipes/binary.rb
@@ -2,7 +2,7 @@ case node[:platform]
 when 'ubuntu'
   apt_repository 'vmware_tools' do
     uri node[:vmware_tools][:base_url] + 'ubuntu' 
-    distribution node['lsb']['codename']
+    distribution node[:lsb][:codename]
     if node[:machine] == 'x86_64'
       arch 'amd64'
     else
@@ -11,10 +11,20 @@ when 'ubuntu'
     components ['main']
     key node[:vmware_tools][:repo_key]
   end
+when 'centos'
+  yum_repository 'vmware_tools' do
+    description "vmware_tools repo"
+    url node[:vmware_tools][:base_url] + "rhel#{node[:platform_version].to_i}/#{node[:kernel][:machine]}"
+    key node[:vmware_tools][:repo_key]
+    notifies :run, "execute[yum_refresh]", :immediately
+  end
+  execute "yum_refresh" do
+    command "yum update"
+#    action :nothing
+  end
 else
   Chef::Log.warning('Platform currently not supported by vmware-tools cookbook')
 end
-
 if node[:vmware_tools][:headless]
   package 'vmware-tools-esx-nox'
 else
